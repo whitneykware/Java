f017a4b2a245b7179bb81338f3495aea119a0759
Whitney Ware <whitneykware@gmail.com>
2018-08-31 10:01:08 -0400

    commit

--- src/lab1/randomRNAseq.java
+++ src/lab1/randomRNAseq.java
@@ -1,10 +1,17 @@
 package lab1;
 
+import java.util.Random;
+
 public class randomRNAseq {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		Random rnd = new Random();
+		
+		int[] m = new int[3];
+		
+		for( int x = 0; x < 1000; x++ )
+			
 	}
 
 }

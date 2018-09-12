adcf8337de9297ce8a73f5c3596dd06d91a51dbe
Whitney Ware <whitneykware@gmail.com>
2018-09-11 21:03:35 -0400

    user chooses number of rounds, exits when incorrect, allows user to quit

--- src/lab2/AminoAcidQuiz.java
+++ src/lab2/AminoAcidQuiz.java
@@ -11,57 +11,51 @@ public class AminoAcidQuiz
 		"P", "S", "T", "W", "Y", "V" };
 
 	public static String[] FULL_NAMES = 
-		{"alanine","arginine", "asparagine", 
-		"aspartic acid", "cysteine",
-		"glutamine",  "glutamic acid",
-		"glycine" ,"histidine","isoleucine",
-		"leucine",  "lysine", "methionine", 
-		"phenylalanine", "proline", 
-		"serine","threonine","tryptophan", 
-		"tyrosine", "valine"};
-	
+		{"Alanine","Arginine", "Asparagine", 
+		"Aspartic acid", "Aysteine",
+		"Glutamine",  "Glutamic acid",
+		"Glycine" ,"Histidine","Isoleucine",
+		"Leucine",  "Lysine", "Methionine", 
+		"Phenylalanine", "Proline", 
+		"Serine","Threonine","Tryptophan", 
+		"Tyrosine", "Valine"};
 	
 	public static void main(String[] args) 
 	{	
 		Random rnd = new Random();
 		
-		System.out.println("Number of rounds: ");
-		String s = System.console().readLine();
-		int questions = Integer.parseInt(s);
-		
 		int score = 0;
-		
-		System.out.println("Please enter the one letter abbreviation for each amino acid given.");
-		
-		
-		for(int x = 0; x < questions; x++)
-		{
-			int a = rnd.nextInt(20);
-			
-			System.out.println(FULL_NAMES[a]);
-			String aString = System.console().readLine().toUpperCase();
-			String aChar = "" + aString.charAt(0);
-			
-			if (aString.equals("QUIT"))
-			{
-				System.out.println("Exiting Quiz.");
-				break;
-			}
-			
-			if (aChar.equals(SHORT_NAMES[a]))
-			{
-				System.out.println("Correct!");
-				score+=1;
-			}
-			else 
-			{
-				System.out.printf("Incorrect. Correct abbreviation for %s is %s", FULL_NAMES[a], SHORT_NAMES[a]);
-				System.out.format("Score: %d", score);
-				break;
-			}
-		}
+		int rounds = Integer.parseInt(System.console().readLine("Number of rounds: "));
+		System.out.println("Please enter the one letter abbreviation for each amino acid given. "
+				+ "Enter 'Quit' to exit the quiz.\n");
 		
 
+			for(int x = 0; x < rounds; x++)
+			{
+				int a = rnd.nextInt(20);
+				
+				System.out.println(FULL_NAMES[a]);
+				String aString = System.console().readLine().toUpperCase();
+				String aChar = "" + aString.charAt(0);
+				
+				if (aString.equals("QUIT"))
+				{
+					System.out.println("Exiting Quiz.");
+					break;
+				}
+				
+				if (aChar.equals(SHORT_NAMES[a]))
+				{
+					System.out.println("Correct!\n");
+					score+=1;
+				}
+				
+				else 
+				{
+					System.out.printf("Incorrect. Correct abbreviation for %s is %s.%n", FULL_NAMES[a], SHORT_NAMES[a]);
+					System.out.printf("Score: %d%n", score);
+					break;
+				}
+			}	
 	}
-
 }

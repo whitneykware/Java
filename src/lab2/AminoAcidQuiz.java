8795791312e1805c000ed255a21426a926779e77
Whitney Ware <whitneykware@gmail.com>
2018-09-16 18:51:20 -0400

    added comments, small wording changes

--- src/lab2/AminoAcidQuiz.java
+++ src/lab2/AminoAcidQuiz.java
@@ -1,3 +1,6 @@
+// Amino acid quiz that allows user to choose number of rounds they would like. 
+// Quiz ends when rounds are over, user enters 'quit', or if answer is incorrect.
+
 package lab2;
 
 import java.util.Random;
@@ -12,7 +15,7 @@ public class AminoAcidQuiz
 
 	public static String[] FULL_NAMES = 
 		{"Alanine","Arginine", "Asparagine", 
-		"Aspartic acid", "Aysteine",
+		"Aspartic acid", "Cysteine",
 		"Glutamine",  "Glutamic acid",
 		"Glycine" ,"Histidine","Isoleucine",
 		"Leucine",  "Lysine", "Methionine", 
@@ -26,36 +29,36 @@ public class AminoAcidQuiz
 		
 		int score = 0;
 		int rounds = Integer.parseInt(System.console().readLine("Number of rounds: "));
-		System.out.println("Please enter the one letter abbreviation for each amino acid given. "
+		System.out.println("Please enter the one letter code for each amino acid given.\n"
 				+ "Enter 'Quit' to exit the quiz.\n");
 		
 
-			for(int x = 0; x < rounds; x++)
+		for(int x = 0; x < rounds; x++)
+		{
+			int a = rnd.nextInt(20);
+				
+			System.out.println(FULL_NAMES[a]);
+			String aString = System.console().readLine().toUpperCase();
+			String aChar = "" + aString.charAt(0);
+				
+			if (aString.equals("QUIT"))
 			{
-				int a = rnd.nextInt(20);
+				System.out.println("Exiting Quiz.");
+				break;
+			}
 				
-				System.out.println(FULL_NAMES[a]);
-				String aString = System.console().readLine().toUpperCase();
-				String aChar = "" + aString.charAt(0);
+			if (aChar.equals(SHORT_NAMES[a]))
+			{
+				score+=1;
+				System.out.printf("Correct!\nScore: %d%n%n", score);
+			}
 				
-				if (aString.equals("QUIT"))
-				{
-					System.out.println("Exiting Quiz.");
-					break;
-				}
-				
-				if (aChar.equals(SHORT_NAMES[a]))
-				{
-					System.out.println("Correct!\n");
-					score+=1;
-				}
-				
-				else 
-				{
-					System.out.printf("Incorrect. Correct abbreviation for %s is %s.%n", FULL_NAMES[a], SHORT_NAMES[a]);
-					System.out.printf("Score: %d%n", score);
-					break;
-				}
-			}	
+			else 
+			{
+				System.out.printf("Incorrect. The correct one letter code for %s is %s.%n", FULL_NAMES[a], SHORT_NAMES[a]);
+				System.out.printf("Final score: %d%n", score);
+				break;
+			}
+		}	
 	}
 }
